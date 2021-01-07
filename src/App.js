b935abbc06a881219405ddf5851b586cceb5f374
Saad Shahid <saad.techgeek@gmail.com>
2021-01-07 12:43:34 +0500

    handlecountrychange implemented

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { fetchData} from './api';
 class App extends React.Component {
     state= {
         data:{},
+        country: '',
     }
 
     async componentDidMount() {
@@ -24,12 +25,20 @@ class App extends React.Component {
     }
 
 
+    handleCountryChange = async (country) => {
+        console.log(country);
+        //fetch the data
+    
+        //set the state
+      }
+
+
     render(){
         const {data} =this.state;
 
         return (<div className={styles.container}>
             <Cards  data={data}/>
-            <CountryPicker/>
+            <CountryPicker handleCountryChange={this.handleCountryChange} />
             <Chart/>
         </div>)
     }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -4,21 +4,25 @@ import {NativeSelect, FormControl} from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
 
-export default function CountryPicker() {
+const Countries = ({ handleCountryChange }) => {
     const [countries, setCountries] = useState([]);
+
     useEffect(() => {
         const fetchAPI = async () => {
           setCountries(await fetchCountries());
         };
     
         fetchAPI();
-      }, []);
+      }, [setCountries]);
     
     return (
         <FormControl className={styles.formControl}>
-            <NativeSelect>
-                <option value="global">Global</option>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="">United States</option>
+                {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
     )
 }
+
+export default Countries;

b292a2450f0af8401b716b6e854e59956fdafd32
Saad Shahid <saad.techgeek@gmail.com>
2021-01-07 12:00:15 +0500

    before chart value entered

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import {Line, Bar} from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 export default function Chart() {
+    const [dailyData,setDailyData] = useState({});
+    //above is same as following in class base component
+    // state= {
+    //     dailyData: {}
+    // }
+
+    //useeffect accept callback
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+        console.log(dailyData);
+        fetchAPI();
+    },[]);
+
+    const lineChart = (
+        dailyData[0]?
+        <Line data={{
+              labels: '',
+              datasets: [{},{}]
+            }}
+        /> : null
+    );
+
     return (
         <div>
             Chart
